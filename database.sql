b209e48f157517d193e6b26ddf438c5ed04764be
shadededen <shadenelsi418@gmail.com>
2025-09-20 09:29:28 -0300

    Atividade finalizada

--- database.sql
+++ database.sql
@@ -1,6 +1,6 @@
-Create Table roupas(
-    Id int primary key auto_increment,
-    categoria varchar(25) NOT NULL,
-    preco varchar(10) NOT NULL,
-    disponivel bit default 1
+Create Table `roupas`(
+    `Id` int primary key auto_increment,
+    `categoria` varchar(25) NOT NULL,
+    `preco` varchar(10) NOT NULL,
+    `disponivel` bit(1) DEFAULT b'1'
 );
